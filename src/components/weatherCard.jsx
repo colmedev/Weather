176a141f67d9640699b06e6c56f1d4a97cb5820a
Miguel Colmenarez <m.colmenarez.dev@gmail.com>
2024-04-24 18:55:45 -0400

    feat: :art: create a cardcomp to the weather data

--- src/components/weatherCard.jsx
+++ src/components/weatherCard.jsx
@@ -1,14 +1,24 @@
 export default function WeatherCard (props) {
+  const Celcius = Math.fround(props.temp - 273.75).toFixed(2)
   return (
     <>
-      <section className="weather">
-        <p>{props.country}</p>
-        <p>{props.city}</p>
-        <p>{props.temp}</p>
-        <p>{props.condition}</p>
-        <p>{props.time}</p>
-        <img src={props.image} alt={props.alt} className="weather-img"/>
-      </section>
+        <div className="location">
+          <p className="country">{props.country}</p>
+          <p className="city">{props.city}</p>
+        </div>
+
+          <div className="cont-img">
+            <img src={props.image} alt={props.alt} className="weather-img"/>
+          </div>
+
+        <div className="date">
+          <p className="cond">{props.condition}</p>
+          <div className="temp">
+            <p>{Celcius}ºC</p>
+            <p>{props.wind} m/s</p>
+            <p>{props.feels_like}</p>
+        </div>
+        </div>
     </>
   )
 }
